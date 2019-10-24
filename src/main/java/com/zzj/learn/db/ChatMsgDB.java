a9b24c4b73c2a45e6fa0bc0fd72f14f4c231fc35
研发-张志军 <zhangzhijun@pansoft.com>
2019-10-24 20:43:19 +0800

    11

---
+++ src/main/java/com/zzj/learn/db/ChatMsgDB.java
@@ -0,0 +1,30 @@
+package com.zzj.learn.db;
+
+import javax.persistence.*;
+import java.util.Date;
+@Entity
+@Table(name = "s_chat_msg")
+public class ChatMsgDB {
+    @Id
+    @PrimaryKeyJoinColumn
+    @Column(updatable = false, nullable = false)
+    private long id;
+    @Column(updatable = false, nullable = false)
+    private String sendUserId;
+    @Column(updatable = false, nullable = false)
+    private String acceptUserId;
+    @Column()
+    private Integer signFlag;
+    @Column(updatable = false, nullable = false)
+    private Date createTime;
+    @Column()
+    private Integer type;
+    @Column()
+    private Integer itemType;
+    @Column()
+    private Integer chatType;
+    @Column()
+    private String msg;
+
+
+}
